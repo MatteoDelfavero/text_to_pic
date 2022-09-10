7de59a7c4e880bacca87fc1fea832ac737c2d443
MatteoDelfavero <matteo.delfavero@gmail.com>
2022-09-10 10:07:05 +0200

    first

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Download CLIP..."
+git clone https://github.com/openai/CLIP                 &> /dev/null
+ 
+echo "Installing VQGAN..."
+git clone https://github.com/CompVis/taming-transformers &> /dev/null
+pip install ftfy regex tqdm omegaconf pytorch-lightning  &> /dev/null
+pip install kornia                                       &> /dev/null
+pip install einops                                       &> /dev/null
+pip install wget                                         &> /dev/null
+ 
+echo "Installing Extra Libraries..."
+pip install stegano                                      &> /dev/null
+apt install exempi                                       &> /dev/null
+pip install python-xmp-toolkit                           &> /dev/null
+pip install imgtag                                       &> /dev/null
+pip install pillow==7.1.2                                &> /dev/null
+ 
+pip install imageio-ffmpeg                               &> /dev/null
+mkdir steps
+echo "Installing Finished!!"
+
+echo "Start Download Face trained files"
+!curl -L -o faceshq.yaml -C - 'https://drive.google.com/uc?export=download&id=1fHwGx_hnBtC8nsq7hesJvs-Klv-P0gzT' #FacesHQ
+!curl -L -o faceshq.ckpt -C - 'https://app.koofr.net/content/links/a04deec9-0c59-4673-8b37-3d696fe63a5d/files/get/last.ckpt?path=%2F2020-11-13T21-41-45_faceshq_transformer%2Fcheckpoints%2Flast.ckpt' #FacesHQ
